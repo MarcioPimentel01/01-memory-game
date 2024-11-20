e4022b6b1e598e65983ecc888379ac2deb7f31a5
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-11-20 16:40:16 -0500

    cards being identifyed with the HTML element data, is being called by dataset.framework

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+
+let firstCard, secondCard;
+
 function flipCard() {
-    console.log(this);
-    console.log(`I was clicked`)
-    this.classList.toggle('flip');
-    console.log(this);
-    console.log(`I was flipped`);
+    this.classList.add('flip');
+
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        secondCard = this;
+        // You can add logic here to check if the two cards match
+        console.log(firstCard.dataset.framework);
+        console.log(secondCard.dataset.framework);
+        hasFlippedCard = false;
+    }
 }
+
 
 
 cards.forEach(card => card.addEventListener('click', flipCard));
